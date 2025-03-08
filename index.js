551b23cdbcf97020b0d8d02f81731c4e053164ec
dipto39 <diptobiswas39@gmail.com>
2025-03-08 22:17:25 +0600

    add static page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
